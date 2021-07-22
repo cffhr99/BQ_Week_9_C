2bf7c6fe5bae8ce7491ee3e3f8ab72225c0ac99f
cffhr99 <83987504+cffhr99@users.noreply.github.com>
2021-07-22 16:41:57 -0400

    work done

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,44 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contributions)
+  - [Testing](#test)
+  - [Questions](#questions)
+  - [Additional Info](#additional-info)
+  ## Description:
+  ${data.description}
+  ## Installation:
+  ${data.installation}
+  ## Usage:
+  ${data.usage}
+  ## License:
+  ${data.license}
+  ## Contribution:
+  ${data.contributions}
+  ## Testing:
+  ${data.test}
+  ## Questions:
+  ${data.questions}
+  ## Additional Info:
+  - Github: [${data.github}](https://github.com/${data.github})
+  - Email: ${data.email}
+`;
+}
+
+module.exports = generateMarkdown;
